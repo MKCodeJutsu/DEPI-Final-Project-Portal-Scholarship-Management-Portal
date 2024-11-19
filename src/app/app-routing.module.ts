c97cf429a5242927a5ce298bb490ddc4cae4ca06
MuhammedAKamel <54419693+MuhammedAKamel@users.noreply.github.com>
2024-11-19 14:46:10 +0200

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { TrackComponent } from './Components/track/track.component';
+import { CommunityComponent } from './Components/community/community.component';
+import { JobOppComponent } from './Components/job-opp/job-opp.component';
+import { ProProfileComponent } from './Components/pro-profile/pro-profile.component';
+import { DepiPointsComponent } from './Components/depi-points/depi-points.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { AuthguardService } from './services/authguard.service';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HomeComponent } from './Components/dashboard/home/home.component';
+import { ProjectComponent } from './Components/dashboard/project/project.component';
+import { QuizComponent } from './Components/dashboard/quiz/quiz.component';
+import { AgendaComponent } from './Components/dashboard/agenda/agenda.component';
+import { LearningComponent } from './Components/dashboard/learning/learning.component';
+import { ConceptsComponent } from './Components/dashboard/concepts/concepts.component';
+import { StreamingComponent } from './Components/dashboard/streaming/streaming.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component:LoginComponent, data: {title: "Login"}},
+  { path: 'track', component:TrackComponent, data: {title: "Track"}, canActivate: [AuthguardService] },
+  { path: 'community', component:CommunityComponent, data: {title: "Community"}, canActivate: [AuthguardService] },
+  { path: 'job', component:JobOppComponent, data: {title: "Job"}, canActivate: [AuthguardService] },
+  { path: 'profile', component:ProProfileComponent, data: {title: "Profile"}, canActivate: [AuthguardService] },
+  { path: 'points', component:DepiPointsComponent, data: {title: "DEPI Points"}, canActivate: [AuthguardService] },
+  { path: 'contact', component:ContactUsComponent, data: {title: "Contact Us"}, canActivate: [AuthguardService] },
+  { path: 'dashboard', component:DashboardComponent, data: {title: "Dashboard"}, canActivate: [AuthguardService] },
+  { path: 'home', component:HomeComponent, data: {title: "Dashboard-Home"}, canActivate: [AuthguardService] },
+  { path: 'agenda',component:AgendaComponent, data: {title: "My Agenda"}, canActivate: [AuthguardService] },
+  { path: 'project', component:ProjectComponent, data: {title: "Ongoing Projects"}, canActivate: [AuthguardService] },
+  { path: 'quiz', component:QuizComponent, data: {title: "Quiz Assessments"}, canActivate: [AuthguardService] },
+  { path: 'learning', component:LearningComponent, data: {title: "Learning Materials"}, canActivate: [AuthguardService] },
+  { path: 'concepts', component:ConceptsComponent, data: {title: "Key Concepts"}, canActivate: [AuthguardService] },
+  { path: 'streaming', component:StreamingComponent, data: {title: "Streaming Lessons"}, canActivate: [AuthguardService] }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DEPI-Final-Project';
+
+  showHeaderAndFooter = false;
+
+  constructor(private router: Router, private titleService: Title) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routeTitle = '';
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+          if (route.snapshot.data['title']) {
+            routeTitle = route!.snapshot.data['title'];
+          }
+          return routeTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`${title}`);
+        }
+      });
+
+
+
+
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Define an array of routes where header and footer should be hidden
+        const routesWithoutHeaderFooter = ['/dashboard', '/home', '/agenda', '/project', '/quiz', '/learning','/concepts', '/streaming'];
+
+        // Check if the current URL matches any of the specified routes
+        this.showHeaderAndFooter = !routesWithoutHeaderFooter.some(route =>
+          event.urlAfterRedirects.startsWith(route)
+        );
+      }
+    });
+  }
 }
+
